622285cdafd880c96f4cd03b0b7354b198d41327
Oleksandr <113306838+OKodovbetskyi@users.noreply.github.com>
2022-12-22 20:28:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000
-
+console.log("hello from docker");
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
