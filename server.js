bfb2361af8048275b6fbd82a8ef6ec4641de127c
Oleksandr <113306838+OKodovbetskyi@users.noreply.github.com>
2022-12-22 22:28:57 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Alex!')
+  res.send('<h1>AUTOMATTEEDDD. 100% EPIC 4. Merry Christmas!</h1>')
 })
 
 app.listen(port, () => {
